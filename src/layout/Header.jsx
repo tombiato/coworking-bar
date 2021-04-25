1590c5fbf95ea3a83dfc5775f87c0a267ceba483
Tom Biato <tom.biato@ynov.com>
2021-04-25 13:56:59 +0200

    Init project

---
+++ src/layout/Header.jsx
@@ -0,0 +1,12 @@
+const Header = () => {
+	return (
+		<div className='flex justify-between bg-blue-400'>
+			<h1>Coworking Bar</h1>
+			<nav>
+				<a href='#'>Home</a>
+			</nav>
+		</div>
+	);
+};
+
+export default Header;
